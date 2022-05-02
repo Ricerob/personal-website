9ebe72024c2f8031c40d2cd856ac08d517bb1738
Robby Rice <riceslif@gmail.com>
2022-05-02 13:23:25 -0500

    Fixed bottom nav

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,19 @@
 import React from "react";
 import './styles/shared.css';
 import './styles/about.css';
+import {Container, Row, Col} from 'react-bootstrap'
 
 function About() {
     return (
-        <h3>About</h3>
+        <Container fluid className="flex-wrapper px-0">
+            <Container fluid>
+            <Row>
+                <Col>
+                <h3>READ ALL ABOUT IT!</h3>
+                </Col>
+            </Row>
+            </Container>
+        </Container>
     );
 }
 
--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -1,7 +1,6 @@
 import React from "react";
 import './styles/navbar.css'
 import { Navbar, Container, Nav } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
 import { Twitter, Github } from 'react-bootstrap-icons';
 
 const BottomNav = () => {
@@ -9,8 +8,8 @@ const BottomNav = () => {
         <Navbar sticky="bottom" className="py-0">
             <Container fluid className="justify-content-end">
                 <Nav>
-                    <Link to="https://twitter.com/rehbudev" className="nav-link"><Twitter width="30" height="30"/></Link>
-                    <Link to="https://github.com/Ricerob" className="nav-link"><Github width="30" height="30"/></Link>
+                    <a href="https://twitter.com/rehbudev" target="_blank" rel="noreferrer noopener" className="nav-link"><Twitter width="30" height="30"/></a>
+                    <a href="https://github.com/Ricerob" target="_blank" rel="noreferrer noopener" className="nav-link"><Github width="30" height="30"/></a>
                 </Nav>
             </Container>
         </Navbar>
