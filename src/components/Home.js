2765cd31b181ea34085ebb942610ef92a529e70b
Robby <riceslif@gmail.com>
2022-05-02 11:54:11 -0500

    added to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import { React, useEffect, useRef} from "react";
 import './styles/shared.css';
 import './styles/home.css';
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Image } from 'react-bootstrap'
 import Typed from "typed.js";
 import LazyHero from "react-lazy-hero/lib/LazyHero";
 
@@ -38,7 +38,21 @@ function Home() {
                 </Row>
             </Container>
             <Container fluid className="intro-container">
-                
+                <Row className="px-5 py-3">
+                    <Col>
+                    <h3>Welcome!</h3>
+                    <p>My name is Robby, a student developer. Thanks for looking around!</p>
+                    <p>I'm currently interested in Web3 tech and building front-ends. In the past, I've worked with
+                        C, Java, assembly, and more. Currently I do most of my out-of-school work in Python and JavaScript.
+                    </p>
+                    <p>
+                        Feel free to snoop around to see my projects. Please reach out - I love meeting new people!
+                    </p>
+                    </Col>
+                    <Col xs lg="2">
+                    <Image fluid id="home-selfie" roundedCircle src="/selfie.JPG"></Image>
+                    </Col>
+                </Row>
             </Container>
         </Container>
         
