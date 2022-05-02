5d53376dbc4a0990b1e80776db5122bcd45e51fb
Robby <riceslif@gmail.com>
2022-05-01 22:14:18 -0500

    Home page w/ hero and typer

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,45 @@
-import React from "react";
+import { React, useEffect, useRef} from "react";
 import './styles/shared.css';
 import './styles/home.css';
-import { Container } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
+import Typed from "typed.js";
+import LazyHero from "react-lazy-hero/lib/LazyHero";
 
 function Home() {
+    const ren = useRef(null);
+
+    useEffect(() => {
+        const typed = new Typed(ren.current, {
+            strings: ["programming", "learning", "building", "saving the world with code!"],
+            startDelay: 300,
+            typeSpeed: 60,
+            backSpeed: 50,
+            backDelay: 1000,
+        });
+
+        return () => {typed.destroy();};
+    }, []);
+
     return (
-        <Container className="flex-wrapper">
-            <h3>home</h3>
+        <Container fluid className="flex-wrapper px-0">
+            <Container fluid id="hero-container">
+                <Row>
+                    <LazyHero imageSrc="/hero2.jpg" color="#FFE8D6" opacity={0.4}
+                        transitionDuration={800} transitionTimingFunction="ease-in-out"
+                        parallaxOffset={10}
+                        />
+                </Row>
+            </Container>
+            <Container className="typer my-4">
+                <Row className="blurb-container">
+                    <Col>
+                        <h3 className="robby-is">Robby is <b><span id="typed-span" ref={ren}></span></b></h3>
+                    </Col>
+                </Row>
+            </Container>
+            <Container fluid className="intro-container">
+                
+            </Container>
         </Container>
         
     );
