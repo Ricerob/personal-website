ea3ba45609cc4d68c1b681071fdd884be5cc2892
Robby Rice <riceslif@gmail.com>
2022-05-19 13:48:38 -0500

    quick 404 page

--- src/components/Missing.js
+++ src/components/Missing.js
@@ -1,10 +1,14 @@
 import React from "react";
 import './styles/shared.css';
 import './styles/missing.css';
+import { Container, Button } from "react-bootstrap";
 
 function Missing() {
     return (
-        <h3>missing</h3>
+        <Container fluid className="flex-wrapper">
+            <h3>404!</h3>
+            <Button href="/">Back to safety!</Button>
+        </Container>
     );
 }
 
